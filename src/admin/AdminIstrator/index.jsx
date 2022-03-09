0c4193431f3f0724ff9163cd46fb556b5e5351a5
Phinv <nguyenvanphi271198@gmail.com>
2022-03-09 13:48:52 +0700

    UI admin

--- src/admin/AdminIstrator/index.jsx
+++ src/admin/AdminIstrator/index.jsx
@@ -1,8 +1,107 @@
 import React from 'react';
 import MasterLayout from 'layouts/MasterLayout';
+import { TitleItem } from './TitleItem';
+import { BoardItem } from 'components/BoardItem';
 
 const Administrator = () => {
-  return <MasterLayout>Administrator</MasterLayout>;
+  return (
+    <MasterLayout>
+      <section className="p-3 flex items-center space-x-4">
+        <div className="w-[15%] border rounded">1</div>
+        <div className="w-[85%] border rounded space-y-4">
+          <div className="space-y-4 border-b p-4 pb-5">
+            <TitleItem title="Draft" number="8" />
+            <div className="grid grid-cols-3 gap-4 px-3">
+              <BoardItem
+                imgPage="https://i.pravatar.cc/100?img=2"
+                imgAvatar="https://i.pravatar.cc/100?img=2"
+                nameProject="12345"
+                admin
+                shadowNone
+                placement="bottomRight"
+              />
+              <BoardItem
+                imgPage="https://i.pravatar.cc/100?img=2"
+                imgAvatar="https://i.pravatar.cc/100?img=2"
+                nameProject="12345"
+                admin
+                shadowNone
+                placement="bottomRight"
+              />
+              <BoardItem
+                imgPage="https://i.pravatar.cc/100?img=2"
+                imgAvatar="https://i.pravatar.cc/100?img=2"
+                nameProject="12345"
+                admin
+                shadowNone
+                placement="bottomRight"
+              />
+            </div>
+          </div>
+
+          <div className="space-y-4 border-b p-4 pb-5">
+            <TitleItem title="Ideas" number="8" />
+            <div className="grid grid-cols-3 gap-4 px-3">
+              <BoardItem
+                imgPage="https://i.pravatar.cc/100?img=2"
+                imgAvatar="https://i.pravatar.cc/100?img=2"
+                nameProject="12345"
+                admin
+                shadowNone
+                placement="bottomRight"
+              />
+              <BoardItem
+                imgPage="https://i.pravatar.cc/100?img=2"
+                imgAvatar="https://i.pravatar.cc/100?img=2"
+                nameProject="12345"
+                admin
+                shadowNone
+                placement="bottomRight"
+              />
+              <BoardItem
+                imgPage="https://i.pravatar.cc/100?img=2"
+                imgAvatar="https://i.pravatar.cc/100?img=2"
+                nameProject="12345"
+                admin
+                shadowNone
+                placement="bottomRight"
+              />
+            </div>
+          </div>
+
+          <div className="space-y-4 p-4 pb-5">
+            <TitleItem title="Project" number="8" />
+            <div className="grid grid-cols-3 gap-4 px-3">
+              <BoardItem
+                imgPage="https://i.pravatar.cc/100?img=2"
+                imgAvatar="https://i.pravatar.cc/100?img=2"
+                nameProject="12345"
+                admin
+                shadowNone
+                placement="bottomRight"
+              />
+              <BoardItem
+                imgPage="https://i.pravatar.cc/100?img=2"
+                imgAvatar="https://i.pravatar.cc/100?img=2"
+                nameProject="12345"
+                admin
+                shadowNone
+                placement="bottomRight"
+              />
+              <BoardItem
+                imgPage="https://i.pravatar.cc/100?img=2"
+                imgAvatar="https://i.pravatar.cc/100?img=2"
+                nameProject="12345"
+                admin
+                shadowNone
+                placement="bottomRight"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    </MasterLayout>
+  );
 };
 
 export default Administrator;
