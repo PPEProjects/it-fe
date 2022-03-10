f37d4fd719bcd9838c7c7e7a7ffbcdfce0422d87
TrongNQ <trongnguyenquoc113@gmail.com>
2022-03-10 09:59:24 +0700

    Code

---
+++ src/admin/AdminIstrator/AllAdmin/Reviewer/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Reviewer = () => {
+  return <div>Reviewer</div>;
+};
--- src/components/BoardItem.jsx
+++ src/components/BoardItem.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { Menu, Dropdown, Modal, Button } from 'antd';
 import { TopComment } from 'pages/home/AllPage/TopComment';
+import { Reviewer } from 'admin/AdminIstrator/AllAdmin/Reviewer';
 import { AssignReviewer } from 'admin/AdminIstrator/AllAdmin/AssignReviewer';
+import { UpdateInformation } from 'admin/AdminIstrator/AllAdmin/UpdateInformation';
 import { Link } from 'react-router-dom';
 import { MenuItemHover } from 'components/MenuItemHover';
 import { thumbImage } from 'services/convert';
@@ -31,6 +33,8 @@ export const BoardItem = ({
 }) => {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [isModalAssignReviewer, setIsModalAssignReviewer] = useState(false);
+  const [isModalReviewer, setIsModalReviewer] = useState(false);
+  const [isModalUpdateInformation, setIsModalUpdateInformation] = useState(false);
 
   const showModal = () => {
     setIsModalVisible(true);
@@ -47,11 +51,24 @@ export const BoardItem = ({
   const handleCancelAssignReviewer = () => {
     setIsModalAssignReviewer(false);
   };
+  const showModalReviewer = () => {
+    setIsModalReviewer(true);
+  };
 
-  const renderModalTopComment = () => {
+  const handleModalReviewer = () => {
+    setIsModalReviewer(false);
+  };
+  const showModalUpdateInformation = () => {
+    setIsModalUpdateInformation(true);
+  };
+
+  const handleCancelUpdateInformation = () => {
+    setIsModalUpdateInformation(false);
+  };
+  const renderModalReviewer = () => {
     return (
-      <Modal visible={isModalVisible} onCancel={handleCancel} footer={null}>
-        <TopComment />
+      <Modal visible={isModalReviewer} onCancel={handleModalReviewer} footer={null}>
+        <Reviewer />
       </Modal>
     );
   };
@@ -63,7 +80,24 @@ export const BoardItem = ({
       </Modal>
     );
   };
-
+  const renderModalTopComment = () => {
+    return (
+      <Modal visible={isModalVisible} onCancel={handleCancel} footer={null}>
+        <TopComment />
+      </Modal>
+    );
+  };
+  const renderModelUpdateInformation = () => {
+    return (
+      <Modal
+        visible={isModalUpdateInformation}
+        onCancel={handleCancelUpdateInformation}
+        footer={null}
+      >
+        <UpdateInformation />
+      </Modal>
+    );
+  };
   const menu = () => {
     return (
       <Menu>
@@ -82,9 +116,9 @@ export const BoardItem = ({
               <MenuItemHover nameMenu="View Description" />
             </Link>
             <MenuItemHover nameMenu="Download" onClick={showModal} />
-            <MenuItemHover nameMenu="Review" />
+            <MenuItemHover nameMenu="Review" onClick={showModalReviewer} />
             <MenuItemHover nameMenu="Assign Reviewer" onClick={showModalAssignReviewer} />
-            <MenuItemHover nameMenu="Update Information" />
+            <MenuItemHover nameMenu="Update Information" onClick={showModalUpdateInformation} />
           </>
         )}
       </Menu>
@@ -99,6 +133,8 @@ export const BoardItem = ({
     >
       {renderModalTopComment()}
       {renderModalAssignReviewer()}
+      {renderModalReviewer()}
+      {renderModelUpdateInformation()}
       <div className="group aspect-w-4 rounded-md relative aspect-h-4 overflow-hidden bg-gray-300">
         <img
           className="h-[215px] w-full object-cover rounded cursor-pointer"
