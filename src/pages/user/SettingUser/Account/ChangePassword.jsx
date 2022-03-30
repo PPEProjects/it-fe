34df7bfb07d1c377fd66b91d9b7f1da8dc1009be
TrongNQ <trongnguyenquoc113@gmail.com>
2022-03-30 08:32:02 +0700

    fix bug

--- src/pages/user/SettingUser/Account/ChangePassword.jsx
+++ src/pages/user/SettingUser/Account/ChangePassword.jsx
@@ -3,6 +3,7 @@ import { Form, Input, Button } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
 import { authsSelector, authChangePassword } from 'pages/auth/authsSlice';
 import { error, success } from 'components';
+import { StarRed } from './StarRed';
 
 export const ChangePassword = () => {
   const dispatch = useDispatch();
@@ -27,61 +28,73 @@ export const ChangePassword = () => {
       layout="vertical"
       className="!p-4 border bg-white space-y-3 rounded-md"
     >
-      <Form.Item
-        label="Current password"
-        name="old_password"
-        className="!mb-0"
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-      >
-        <Input.Password className="!rounded" placeholder="Password" size="large" />
-      </Form.Item>
-      <Form.Item
-        name="new_password"
-        label="New password"
-        className="!mb-0"
-        hasFeedback
-        rules={[
-          {
-            pattern: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/,
-            message:
-              'Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters!',
-          },
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-      >
-        <Input.Password className="!rounded" placeholder="Enter your new password" size="large" />
-      </Form.Item>
-      <Form.Item
-        name="confirm_password"
-        label="Confirm password"
-        dependencies={['new_password']}
-        className="!mb-0"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-          ({ getFieldValue }) => ({
-            validator(_, value) {
-              if (!value || getFieldValue('new_password') === value) {
-                return Promise.resolve();
-              }
-              return Promise.reject(new Error('The two passwords that you entered do not match!'));
+      <div>
+        <StarRed star name="Current password" />
+        <Form.Item
+          name="old_password"
+          className="!mb-0"
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
             },
-          }),
-        ]}
-      >
-        <Input.Password className="!rounded" placeholder="Confirm your new password" size="large" />
-      </Form.Item>
+          ]}
+        >
+          <Input.Password className="!rounded" placeholder="Password" size="large" />
+        </Form.Item>
+      </div>
+      <div>
+        <StarRed star name="New password" />
+        <Form.Item
+          name="new_password"
+          className="!mb-0"
+          hasFeedback
+          rules={[
+            {
+              pattern: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/,
+              message:
+                'Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters!',
+            },
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+        >
+          <Input.Password className="!rounded" placeholder="Enter your new password" size="large" />
+        </Form.Item>
+      </div>
+      <div>
+        <StarRed star name="Confirm password" />
+        <Form.Item
+          name="confirm_password"
+          dependencies={['new_password']}
+          className="!mb-0"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue('new_password') === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(
+                  new Error('The two passwords that you entered do not match!')
+                );
+              },
+            }),
+          ]}
+        >
+          <Input.Password
+            className="!rounded"
+            placeholder="Confirm your new password"
+            size="large"
+          />
+        </Form.Item>
+      </div>
       <div>
         <Form.Item className="!mb-0 p-3 text-right">
           <Button
