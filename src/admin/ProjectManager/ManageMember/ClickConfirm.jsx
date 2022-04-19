d86c45ca7b61129cb07b450016c1f5dc80c2c1be
datvnt <thanhdatvongoc@gmail.com>
2022-04-19 11:12:06 +0700

    done manage

--- src/admin/ProjectManager/ManageMember/ClickConfirm.jsx
+++ src/admin/ProjectManager/ManageMember/ClickConfirm.jsx
@@ -3,12 +3,21 @@ import { LabelItemProject } from 'pages/project/NewProject/LabelItemProject';
 import { createProject, projectSelector } from 'pages/project/projectSlice';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { upsertMemberProject, memberProjectSelector } from 'pages/memberProject/memberProjectSlice';
 
-export const ClickConfirm = () => {
+export const ClickConfirm = ({ item, closeModal }) => {
   const dispatch = useDispatch();
-  // const { cProject } = useSelector(projectSelector);
+  const { cCreateProjectMembers } = useSelector(memberProjectSelector);
   const [form] = Form.useForm();
   const { TextArea } = Input;
+
+  useEffect(() => {
+    form.setFieldsValue({
+      data: {
+        projectId: item?.project?.id,
+      },
+    });
+  }, [item]);
 
   // useEffect(() => {
   //   dispatch(createProject());
@@ -23,25 +32,30 @@ export const ClickConfirm = () => {
         form={form}
         name="basic"
         // onFinish={values => dispatch(createProject(values))}
-        onFinish={values => console.log('values', values)}
+        // onFinish={values => console.log('values', values)}
+        onFinish={values => dispatch(upsertMemberProject(values))}
         scrollToFirstError
         layout="vertical"
       >
         <Form.List name={`data`}>
           {() => (
             <>
+              <Form.Item label="Project Id" name="projectId" hidden>
+                <Input className="!rounded !w-1/2 !h-10" />
+              </Form.Item>
               <Form.Item label="Position" name="position">
                 <Input className="!rounded !w-1/2 !h-10" />
               </Form.Item>
-              <Form.Item label="Job Description" name="description">
-                <TextArea rows={4} maxLength={6} className="!round-lg" />
+              <Form.Item label="Job Description" name="jobDescription">
+                <TextArea rows={4} className="!round-lg" />
               </Form.Item>
-              <Form.Item label="Link test" name="link">
+              <Form.Item label="Link test" name="linkTest">
                 <Input className="!rounded !h-10" />
               </Form.Item>
               <Form.Item className="text-right">
                 <Button
-                  // loading={cProject.isLoading}
+                  // loading={cCreateProjectMembers?.isLoading}
+                  onClick={() => closeModal()}
                   type="primary"
                   className="!h-10 !rounded-lg"
                   htmlType="submit"
