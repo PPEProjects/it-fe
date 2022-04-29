54885efb7b6927930d6551252ba324c28de6c4de
datvnt <thanhdatvongoc@gmail.com>
2022-04-29 13:28:57 +0700

    fix project-review

--- src/admin/AdminIstrator/ProjectReview/MyProjects.jsx
+++ src/admin/AdminIstrator/ProjectReview/MyProjects.jsx
@@ -11,7 +11,7 @@ export const MyProjects = () => {
   const { mlMyIdeaProject, upStatusProject } = useSelector(projectSelector);
 
   useEffect(() => {
-    dispatch(detailProjectMemberByPosition('pr'));
+    dispatch(detailProjectMemberByPosition('project_review'));
   }, [dispatch, upStatusProject]);
 
   const filterIdeas = (mlMyIdeaProject?.myIdeaProject ?? []).filter(
